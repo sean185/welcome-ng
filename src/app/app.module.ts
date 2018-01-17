1d26f56d30577bc22a6b2ab6d8df49b9df27836f
Sean Ang <sean@ort.solutions>
2018-01-17 23:29:11 +0800

    testing pug imports and replicating previous UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { SuiModule } from 'ng2-semantic-ui';
+
 
 
 import { AppComponent } from './app.component';
-
+import { BasicRoutingModule } from './basic-routing.module';
+import { LandingComponent } from './landing/landing.component';
+import { OverlayComponent } from './overlay/overlay.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    OverlayComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SuiModule,
+    BasicRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/basic-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+
+const routes: Routes = [
+  { path: '', component: LandingComponent},
+  // { path: 'details/:name', component: DetailsComponent },
+  // { path: '**', component: NotFoundComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class BasicRoutingModule { }
